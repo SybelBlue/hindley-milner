a922397b0755c2b850d064e656ff64393a0a4930
Logan Caraco <lbc.dna@gmail.com>
2022-05-07 12:45:09 -0700

    unification

--- src/Inference.hs
+++ src/Inference.hs
@@ -6,10 +6,13 @@ import Context
 import Substitution
 import Types 
 
-import Control.Exception
 import Control.Monad.Except
 import Control.Monad.State
 
+data TypeError 
+    = UnificationFail Type Type
+    | InfiniteType TVar Type
+    | UnboundVariable String
 
 newtype Unique = Unique { count :: Int }
 
@@ -34,3 +37,21 @@ fresh = do
   s <- get
   put s{count = count s + 1}
   return $ TVar $ TV (letters !! count s)
+
+unify :: Type -> Type -> Infer Subst
+unify (l `TArr` r) (l' `TArr` r') = do
+    s1 <- unify l l'
+    s2 <- unify (apply s1 r) (apply s1 r')
+    return (s2 `compose` s1)
+
+unify (TVar a) t = bind a t
+unify t (TVar a) = bind a t
+
+unify (TCon a) (TCon b) | a == b = return nullSubst
+
+unify t1 t2 = throwError $ UnificationFail t1 t2
+
+bind :: TVar -> Type -> Infer Subst
+bind a t | t == TVar a      = return nullSubst
+         | occursCheck a t  = throwError $ InfiniteType a t
+         | otherwise        = return $ Map.singleton a t
