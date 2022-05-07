cd0d9fb6be7ba11d01c2db5007b4760c5a5c0629
Logan Caraco <lbc.dna@gmail.com>
2022-05-07 12:32:00 -0700

    substitutions

--- src/Substitution.hs
+++ src/Substitution.hs
@@ -2,7 +2,8 @@ module Substitution where
 
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
-import Types (TVar, Type)
+import Types
+import Context (TypeEnv(..))
 
 type Subst = Map.Map TVar Type
 
@@ -13,5 +14,30 @@ compose :: Subst -> Subst -> Subst
 s1 `compose` s2 = Map.map (apply s1) s2 `Map.union` s1
 
 class Substitutable a where
+    -- | apply substitution over a
     apply :: Subst -> a -> a
+    -- | get the free type variables
     ftv   :: a -> Set.Set TVar
+
+
+instance Substitutable Type where
+    apply _ (TCon a)        = TCon a
+    apply s t@(TVar a)      = Map.findWithDefault t a s
+    apply s (t1 `TArr` t2)  = apply s t1 `TArr` apply s t2
+
+    ftv TCon{}         = Set.empty
+    ftv (TVar a)       = Set.singleton a
+    ftv (t1 `TArr` t2) = ftv t1 `Set.union` ftv t2
+
+instance Substitutable Scheme where
+    apply s (Forall as t) = Forall as $ apply s' t
+                            where s' = foldr Map.delete s as
+    ftv (Forall as t) = ftv t `Set.difference` Set.fromList as
+
+instance Substitutable a => Substitutable [a] where
+    apply = fmap . apply
+    ftv   = foldr (Set.union . ftv) Set.empty
+
+instance Substitutable TypeEnv where
+    apply s (TypeEnv env) = TypeEnv $ Map.map (apply s) env
+    ftv (TypeEnv env) = ftv $ Map.elems env
