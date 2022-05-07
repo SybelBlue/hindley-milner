f93b3a758390191c7ec336a4e34ec556f1f02659
Logan Caraco <lbc.dna@gmail.com>
2022-05-06 21:44:10 -0700

    adding substitutable

--- src/Substitution.hs
+++ src/Substitution.hs
@@ -1,6 +1,17 @@
 module Substitution where
 
 import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 import Types (TVar, Type)
 
 type Subst = Map.Map TVar Type
+
+nullSubst :: Subst
+nullSubst = Map.empty
+
+compose :: Subst -> Subst -> Subst
+s1 `compose` s2 = Map.map (apply s1) s2 `Map.union` s1
+
+class Substitutable a where
+    apply :: Subst -> a -> a
+    ftv   :: a -> Set.Set TVar
