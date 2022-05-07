a73e18e6fef6b51ad4e3c67eb253a43843420b7b
Logan Caraco <lbc.dna@gmail.com>
2022-05-06 21:25:58 -0700

    added typeenv

--- src/Types.hs
+++ src/Types.hs
@@ -1,5 +1,9 @@
 module Types where
 
+import Syntax ( Var )
+
+import qualified Data.Map.Strict as Map
+
 newtype TVar = TV String
     deriving (Show, Eq, Ord)
 
@@ -15,3 +19,5 @@ typeInt = TCon "Int"
 typeBool = TCon "Bool"
 
 data Scheme = Forall [TVar] Type
+
+newtype TypeEnv = TypeEnv (Map.Map Var Scheme)
